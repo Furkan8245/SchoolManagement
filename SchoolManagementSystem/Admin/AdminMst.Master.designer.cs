f91d91ffe1262b7ee52e61a03962306a723c2f62
Furkan Keleş <furkan.keles.8245@hotmail.com>
2024-07-20 19:48:51 +0300

    Varsayılan belge eklendi ve dizin tarama etkinleştirildi.

---
+++ SchoolManagementSystem/Admin/AdminMst.Master.designer.cs
@@ -0,0 +1,53 @@
+﻿//------------------------------------------------------------------------------
+// <otomatik olarak oluşturulmuş>
+//     Bu kod bir araç tarafından oluşturuldu.
+//
+//     Bu dosyada yapılacak değişiklikler hatalı davranışa neden olabilir ve
+//     kod tekrar üretildi. 
+// </otomatik olarak oluşturulmuş>
+//------------------------------------------------------------------------------
+
+namespace SchoolManagementSystem.Admin
+{
+
+
+    public partial class AdminMst
+    {
+
+        /// <summary>
+        /// head denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik olarak oluşturulan alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
+
+        /// <summary>
+        /// form1 denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik olarak oluşturulan alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// btnLogOut denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik olarak oluşturulan alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton btnLogOut;
+
+        /// <summary>
+        /// ContentPlaceHolder1 denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik olarak oluşturulan alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+    }
+}
---
+++ SchoolManagementSystem/Models/CommonFn.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace SchoolManagementSystem.Models
+{
+    public class CommonFn
+    {
+        public class Commonfnx
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["SchoolCS"].ConnectionString);
+            public void Query(string query)
+            {
+                if (con.State == ConnectionState.Closed)
+                {
+                    
+                }
+
+            }
+        }
+    }
+}
